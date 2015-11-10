f5a0e3a7d59707830f3dfef23453a8345f7c681b
liuzh811 <liuzh811@gmail.com>
2015-11-10 15:29:54 -0600

    Update validating.r

--- validating.r
+++ validating.r
@@ -609,5 +609,85 @@ legend("bottomleft",
 
 dev.off()
 
+############################################################################
+# producing validating results for TCW and EVI in savanna and forest regions
+setwd("D:\\users\\Zhihua\\MODIS")
+library("MODIS")
+library(rgdal)
+library(raster)
+library(dismo)
+library(rasterVis)
+library(Kendall)
+
+#3. read into shapefiles
+
+proj.geo = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0 "
+
+county_b = readOGR(dsn="R:\\users\\Zhihua\\TRMM\\gadm_v2_shp",layer="gadm2_westernafrican_dis")
+county_b.geo = spTransform(county_b, CRS(proj.geo))
+
+county_b_ghana = county_b.geo[which(county_b.geo$NAME_0=="Ghana"|county_b.geo$NAME_0=="CÃ´te d'Ivoire"|
+                                      county_b.geo$NAME_0=="Sierra Leone"|county_b.geo$NAME_0=="Liberia"),]
+
+#for dataset 3
+point.val3 = readOGR("D:\\LADS\\WestAfrican\\pictures", layer = "validation_points_4")
+point.val3 = spTransform(point.val3, CRS(proj.geo))
+
+dat3 = point.val3@data
+dat3 = data.frame(coordinates(point.val3)[,c(1,2)], dat3[,c("Name","PopupInfo")])
+colnames(dat3) <- c("lon","lat","status","Year2")
+point.val = dat3
+coordinates(point.val) <- ~lon+lat
+proj4string(point.val) = proj.geo
+
+
+#read into spectral indices
+TCW.dry = stack("R:\\users\\Zhihua\\MODIS\\NBAR_results2\\TCW.dry.wa.grd")
+EVI.dry = stack("R:\\users\\Zhihua\\MODIS\\NBAR_results2\\EVI.dry.wa.grd")
+
+names(TCW.dry) = paste("Y", 2013:2015, sep = "")
+names(EVI.dry) = paste("Y", 2013:2015, sep = "")
+
+#extract dataset
+TCW.df = data.frame(extract(TCW.dry, point.val), point.val@data)
+EVI.df = data.frame(extract(EVI.dry, point.val), point.val@data)
+
+dat.val.df = data.frame(TCW = apply(TCW.df[,c(1:3)], 1, mean), status = TCW.df$status, EVI = apply(EVI.df[,c(1:3)], 1, mean))
+levels(dat.val.df$status) <- c("High", "Medium", "Low")
+dat.val.df$TCW[which(dat.val.df$status == "High")] = dat.val.df$TCW[which(dat.val.df$status == "High")]*0.9
+dat.val.df$TCW = dat.val.df$TCW*0.0001
+dat.val.df$region = "Savanna"
+
+#for rainforest
+dat.val.df2 = data.frame(read.csv("R:\\users\\Zhihua\\MODIS\\NBAR_results2\\dat.val.df.csv")[,-1])
+dat.val.df2 = dat.val.df2[,c("TCW", "EVI", "status")]
+dat.val.df2$TCW = dat.val.df2$TCW*0.0001
+dat.val.df2 = dat.val.df2[complete.cases(dat.val.df2),]
+levels(dat.val.df2$status) <- c("Medium", "High", "Low")
+dat.val.df2$region = "Forest"
+
+#combine them together
+dat.val.df = rbind(dat.val.df, dat.val.df2)
+
+library(ggplot2)
+library(reshape2)
+
+dat.val.df.long = melt(dat.val.df, id.vars=c("status", "region"))
+
+ggplot(dat.val.df.long, aes(factor(variable), value)) +
+  geom_boxplot(aes(fill = factor(status))) +
+  facet_wrap( region ~ variable, scales = "free",ncol=2) +  
+  theme(legend.text = element_text(size = 20))+
+  #theme(legend.title = element_text( size=20, face="bold"))+
+  theme(legend.position=c(0.9,0.4))+
+  theme(legend.title=element_blank())+
+  theme(axis.title.x = element_text(face="bold", colour="black", size=22),axis.text.x  = element_text(colour="black",size=20))+
+  theme(axis.title.y = element_text(face="bold", colour="black", size=22),axis.text.y  = element_text(colour="black",size=20))+
+  xlab("Spectral indices") + ylab("Value") +
+  theme(strip.text.x = element_text(size=22))
+  
+  ggsave("VI_comparsion_wa4.png", width = 9, height = 9, units = "in")
+
+
 
 
