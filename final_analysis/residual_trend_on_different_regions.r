49adec15d467cbf505dd24d63746136461c42613
liuzh811 <liuzh811@gmail.com>
2015-12-31 17:05:16 -0600

    Update residual_trend_on_different_regions.r

--- final_analysis/residual_trend_on_different_regions.r
+++ final_analysis/residual_trend_on_different_regions.r
@@ -1,1 +1,5 @@
+########## section 1: calculate 3 by 3 moving windos trends ################
+########## section 2: plot trend ################
+########## section 3: calculate trend on different regions ################
+
 
