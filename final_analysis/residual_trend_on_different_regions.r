2ecaed65c04f8dbbc59834d80c628600e610e643
liuzh811 <liuzh811@gmail.com>
2016-01-04 09:22:39 -0600

    Update residual_trend_on_different_regions.r

--- final_analysis/residual_trend_on_different_regions.r
+++ final_analysis/residual_trend_on_different_regions.r
@@ -273,7 +273,7 @@ ggsave(".\\NBAR_results4\\residual.trend_EVI&TCW_protect.png", width = 10, heigh
 
 # by protected status * country
 lc_rc1 = county_b_ghana.grd*10000 + protected.grd*100 + TCW.trd2.grd 
-lc_rc1.df = data.frame(freq(lc_rc1))[-31,]
+lc_rc1.df = data.frame(freq(lc_rc1))[-40,] #
 lc_rc1.df$lc = floor(lc_rc1.df$value/10000)
 lc_rc1.df$protect = floor((lc_rc1.df$value - lc_rc1.df$lc*10000)/100)
 lc_rc1.df$trend = lc_rc1.df$value - lc_rc1.df$lc*10000-lc_rc1.df$protect*100
@@ -281,7 +281,7 @@ lc_sum = aggregate(count~protect+lc, data = lc_rc1.df, FUN = "sum")
 lc_rc1.df$prop = 100*lc_rc1.df$count/rep(lc_sum$count, each = 3)
 
 lc_rc2 = county_b_ghana.grd*10000 + protected.grd*100 + EVI.trd2.grd 
-lc_rc2.df = data.frame(freq(lc_rc2))[-31,]
+lc_rc2.df = data.frame(freq(lc_rc2))[-40,]
 lc_rc2.df$lc = floor(lc_rc2.df$value/10000)
 lc_rc2.df$protect = floor((lc_rc2.df$value - lc_rc2.df$lc*10000)/100)
 lc_rc2.df$trend = lc_rc2.df$value - lc_rc2.df$lc*10000-lc_rc2.df$protect*100
@@ -299,7 +299,7 @@ levels(lc_rc1.df.long$trend) <- c("Negative", "Positive", "No Trend")
 lc_rc1.df.long$lc = factor(lc_rc1.df.long$lc)
 levels(lc_rc1.df.long$lc) <- c("CÃ´te d'Ivoire", "Ghana", "Guinea", "Liberia", "Sierra Leone")
 lc_rc1.df.long$protect = factor(lc_rc1.df.long$protect)
-levels(lc_rc1.df.long$protect) <- c("Protect", "Non-Protect")
+levels(lc_rc1.df.long$protect) <- c("Reserve", "Eco-Reserve","Non-Protected")
 
 color1 = c("#fb6a4a", "#67a9cf", "#cccccc")
 
@@ -324,3 +324,57 @@ ggplot(data=lc_rc1.df.long, aes(x=protect, y=value, fill=trend)) +
 
 ggsave(".\\NBAR_results4\\residual.trend_EVI&TCW_Protect&country.png", width = 7.5, height = 9, units = "in")
 
+# by protected status * ecoregions
+lc_rc1 = eco.sp.grd*10000 + protected.grd*100 + TCW.trd2.grd 
+lc_rc1.df = data.frame(freq(lc_rc1))[-46,] #
+lc_rc1.df$lc = floor(lc_rc1.df$value/10000)
+lc_rc1.df$protect = floor((lc_rc1.df$value - lc_rc1.df$lc*10000)/100)
+lc_rc1.df$trend = lc_rc1.df$value - lc_rc1.df$lc*10000-lc_rc1.df$protect*100
+lc_sum = aggregate(count~protect+lc, data = lc_rc1.df, FUN = "sum")
+lc_rc1.df$prop = 100*lc_rc1.df$count/rep(lc_sum$count, each = 3)
+
+lc_rc2 = eco.sp.grd*10000 + protected.grd*100 + EVI.trd2.grd 
+lc_rc2.df = data.frame(freq(lc_rc2))[-46,]
+lc_rc2.df$lc = floor(lc_rc2.df$value/10000)
+lc_rc2.df$protect = floor((lc_rc2.df$value - lc_rc2.df$lc*10000)/100)
+lc_rc2.df$trend = lc_rc2.df$value - lc_rc2.df$lc*10000-lc_rc2.df$protect*100
+lc_sum = aggregate(count~protect+lc, data = lc_rc2.df, FUN = "sum")
+lc_rc2.df$prop = 100*lc_rc2.df$count/rep(lc_sum$count, each = 3)
+
+lc_rc.df = rbind(data.frame(lc_rc1.df, VI = "TCW"), 
+                 data.frame(lc_rc2.df, VI = "EVI"))
+
+lc_rc.df = lc_rc.df[-which(lc_rc.df$lc == 5), ] #remove class 5
+
+lc_rc1.df.long = melt(lc_rc.df[,c("lc","protect","trend","prop","VI")], id.vars=c("lc", "protect","trend","VI"))
+
+lc_rc1.df.long$trend = factor(lc_rc1.df.long$trend)
+levels(lc_rc1.df.long$trend) <- c("Negative", "Positive", "No Trend")
+lc_rc1.df.long$lc = factor(lc_rc1.df.long$lc)
+levels(lc_rc1.df.long$lc) <- c("Western Guinean\n Lowland Forests", "Eastern Guinean\n Forests", 
+                               "Guinean Forest\n-Savanna Mosaic", "West Sudanian\n Savanna")
+lc_rc1.df.long$protect = factor(lc_rc1.df.long$protect)
+levels(lc_rc1.df.long$protect) <- c("Reserve", "Eco-Reserve","Non-Protected")
+
+color1 = c("#fb6a4a", "#67a9cf", "#cccccc")
+
+ggplot(data=lc_rc1.df.long, aes(x=protect, y=value, fill=trend)) +
+  geom_bar(stat="identity", position=position_dodge()) + 
+  facet_grid(lc ~ VI) +
+  xlab("") + ylab("Percentage of Land Cover") +
+  theme(axis.ticks = element_blank())+
+  theme(axis.title.x = element_text(face="bold", colour="black", size=22),axis.text.x  = element_text(colour="black",size=20))+
+  theme(axis.title.y = element_text(face="bold", colour="black", size=22),axis.text.y  = element_text(colour="black",size=20))+
+  theme(legend.position=c(0.75,0.1))+
+  theme(legend.text = element_text(size = 18)) +
+  theme(legend.title=element_blank()) +
+  theme(strip.text.x = element_text(size=22))+ 
+  theme(strip.text.y = element_text(size=22))+ 
+  #theme(axis.ticks = element_blank(), axis.text.x = element_blank()) + 
+  scale_fill_manual(values=color1, 
+                    name="",
+                    breaks=levels(lc_rc.df.long$trend),
+                    labels=levels(lc_rc.df.long$trend)) +
+  guides(fill=guide_legend(ncol=1))
+
+ggsave(".\\NBAR_results4\\residual.trend_EVI&TCW_Protect&Ecoregion.png", width = 9, height = 9, units = "in")
