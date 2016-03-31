62ecb278f772de0b25231585b926785ec95056f7
liuzh811 <liuzh811@gmail.com>
2016-03-31 16:31:28 -0500

    Update residual_trend_on_different_regions.r

--- final_analysis/residual_trend_on_different_regions.r
+++ final_analysis/residual_trend_on_different_regions.r
@@ -823,6 +823,82 @@ ggplot(data=lc_rc1.df.long, aes(x=protect, y=value, fill=trend)) +
 ggsave(".\\NBAR_results4\\residual.trend_ghana_cold_evi.png", width = 10, height = 7.5, units = "in")
 
 
+# by country * ecoregions, and adding mean for all the region in 3/31/2016
+lc_rc1 = eco.sp.grd*10000 + county_b_ghana.grd*100 + TCW.trd2.grd 
+lc_rc1.df = data.frame(freq(lc_rc1))[-57,] #
+lc_rc1.df$lc = floor(lc_rc1.df$value/10000)
+lc_rc1.df$country = floor((lc_rc1.df$value - lc_rc1.df$lc*10000)/100)
+lc_rc1.df$trend = lc_rc1.df$value - lc_rc1.df$lc*10000-lc_rc1.df$country*100
+lc_sum = data.frame(aggregate(count~country+lc, data = lc_rc1.df, FUN = "sum"))
+test = merge.data.frame(lc_rc1.df, lc_sum, by = c("lc", "country"))
+test$prop = 100*test$count.x/test$count.y
+lc_rc1.df = test
+
+lc_rc2 = eco.sp.grd*10000 + county_b_ghana.grd*100 + EVI.trd2.grd 
+lc_rc2.df = data.frame(freq(lc_rc2))[-57,]
+lc_rc2.df$lc = floor(lc_rc2.df$value/10000)
+lc_rc2.df$country = floor((lc_rc2.df$value - lc_rc2.df$lc*10000)/100)
+lc_rc2.df$trend = lc_rc2.df$value - lc_rc2.df$lc*10000-lc_rc2.df$country*100
+lc_sum = data.frame(aggregate(count~country+lc, data = lc_rc2.df, FUN = "sum"))
+test = merge.data.frame(lc_rc2.df, lc_sum, by = c("lc", "country"))
+test$prop = 100*test$count.x/test$count.y
+lc_rc2.df = test
+
+lc_rc.df = rbind(data.frame(lc_rc1.df, VI = "TCW"), 
+                 data.frame(lc_rc2.df, VI = "EVI"))
+
+lc_rc.df = lc_rc.df[-which(lc_rc.df$lc == 5), ] #remove class 5
+
+lc_rc1.df.long = melt(lc_rc.df[,c("lc","country","trend","prop","VI")], id.vars=c("lc", "country","trend","VI"))
+
+#
+lc_rc1.df.long$trend = factor(lc_rc1.df.long$trend)
+levels(lc_rc1.df.long$trend) <- c("Negative", "Positive", "No Trend")
+#change the order of the trend
+lc_rc1.df.long$trend <- ordered(lc_rc1.df.long$trend, levels = c("Negative", "No Trend", "Positive"))
+
+lc_rc1.df.long$lc = factor(lc_rc1.df.long$lc)
+levels(lc_rc1.df.long$lc) <- c("Western Guinean\n Lowland Forests", "Eastern Guinean\n Forests", 
+                               "Guinean Forest\n-Savanna Mosaic", "West Sudanian\n Savanna")
+levels(lc_rc1.df.long$lc) <- c("WGLF", "EGF", "GFSM", "WSS")                               
+#change the order of the ecoregion
+lc_rc1.df.long$lc <- ordered(lc_rc1.df.long$lc, levels = c("WSS", "GFSM", "EGF","WGLF"))
+
+lc_rc1.df.long$country = factor(lc_rc1.df.long$country)
+levels(lc_rc1.df.long$country) <- c("CÃ´te d'Ivoire", "Ghana", "Guinea", "Liberia", "Sierra Leone")
+levels(lc_rc1.df.long$country) <- c("CIV", "GHA", "GIN", "LBR", "SLE")
+
+lc_rc1.df.long$country <- ordered(lc_rc1.df.long$country, levels = c("GIN", "SLE", "LBR","CIV","GHA"))
+
+#color1 = c("#fb6a4a", "#67a9cf", "#cccccc"): color order for c("Negative", "Positive", "No Trend")
+color1 = c("#fb6a4a", "#cccccc", "#67a9cf") # color order for c("Negative", "No Trend","Positive")
+
+#remove conutry == LBR & lc == GFSM
+lc_rc1.df.long = lc_rc1.df.long[-which(lc_rc1.df.long$country == "LBR" & lc_rc1.df.long$lc == "GFSM"), ]
+
+
+ggplot(data=lc_rc1.df.long, aes(x=country, y=value, fill=trend)) +
+  geom_bar(stat="identity", position=position_dodge()) + 
+  facet_grid(lc ~ VI) +
+  xlab("") + ylab("Percentage of Land Cover") +
+  theme(axis.ticks = element_blank())+
+  theme(axis.title.x = element_text(face="bold", colour="black", size=22),axis.text.x  = element_text(colour="black",size=20))+
+  theme(axis.title.y = element_text(face="bold", colour="black", size=22),axis.text.y  = element_text(colour="black",size=20))+
+  #theme(legend.position=c(0.7,0.45))+
+  theme(legend.position="top")+
+  theme(legend.text = element_text(size = 18)) +
+  theme(legend.title=element_blank()) +
+  theme(strip.text.x = element_text(size=20))+ 
+  theme(strip.text.y = element_text(size=20))+ 
+  #theme(axis.ticks = element_blank(), axis.text.x = element_blank()) + 
+  scale_fill_manual(values=color1, 
+                    name="",
+                    breaks=levels(lc_rc1.df.long$trend),
+                    labels=levels(lc_rc1.df.long$trend)) +
+  guides(fill=guide_legend(ncol=3))
+
+ggsave(".\\NBAR_results4\\residual.trend_EVI&TCW_ecoregion-country.png", width = 10, height = 7.5, units = "in")
+
 #plot some highlighted area
 regions = readOGR(dsn="R:/users/Zhihua/MODIS/NBAR_results3", layer="Val_regions")
 projection(regions) <- proj.geo 
